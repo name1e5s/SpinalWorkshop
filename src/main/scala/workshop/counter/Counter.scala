dd2b14e5376f18a6e73af214045ae22b6bcb758b
name1e5s <name1e5s@qq.com>
2022-02-07 22:14:35 +0800

    counter

--- src/main/scala/workshop/counter/Counter.scala
+++ src/main/scala/workshop/counter/Counter.scala
@@ -8,6 +8,11 @@ case class Counter(width: Int) extends Component {
     val value    = out UInt(width bits)
     val full     = out Bool()
   }
+  val value_reg = Reg(UInt(width bits))
+  val value_next = Mux(io.clear, U(0), value_reg + 1)
 
-  //TODO define the logic
+  value_reg := value_next
+
+  io.value := value_reg
+  io.full := value_reg.andR
 }
